ecd2c145b66951576124f15cd6ae11950efcc823
mbelur <mbelur@suse.com>
2021-04-02 10:34:55 -0700

    More unittests

--- next_generation/pint_server/tests/unit/mock_pint_data.py
+++ next_generation/pint_server/tests/unit/mock_pint_data.py
@@ -15,6 +15,7 @@ def images(provider):
         image_list.append(dict(image.attrib))
     return image_list
 
+
 def servers(provider):
     base_path = os.path.dirname(os.path.abspath(__file__))
     data_file = glob.glob(base_path + '/mockpintdata/' + provider + '.xml')
@@ -41,6 +42,20 @@ def filter_mocked_return_value_images_by_state(images, img_state):
             img_list.append(image)
     return img_list
 
+def filter_mocked_return_value_images_region(images, region):
+    img_list = []
+    for image in images:
+        if image.get('region') == region:
+            img_list.append(image)
+    return img_list
+
+def filter_mocked_return_value_servers_region(servers, region):
+    srv_list = []
+    for server in servers:
+        if server.get('region') == region:
+            srv_list.append(server)
+    return srv_list
+
 def construct_mock_expected_response(content_dict, collection_name):
     if collection_name:
         content = {collection_name: content_dict}
@@ -54,14 +69,20 @@ expected_json_providers = {'providers': [
     ]
 }
 
-mocked_return_value_server_types = ['region', 'update']
-expected_json_server_types = {'types': ['region', 'update']}
+mocked_return_value_image_states = [{"name":"active"},{"name":"deleted"},{"name":"deprecated"},{"name":"inactive"}]
+expected_json_image_states = {"states":[{"name":"active"},{"name":"deleted"},{"name":"deprecated"},{"name":"inactive"}]}
+
+mocked_return_value_server_types = [{"name": "region"},{"name": "update"}]
+expected_json_server_types = {'types': [{'name': 'region'}, {'name': 'update'}]}
 
 mocked_return_value_images = {}
 expected_json_images = {}
 
 mocked_return_value_servers = {}
 expected_json_servers = {}
+
+mocked_return_value_regions = {}
+expected_json_regions = {}
 
 #Images
 mocked_return_value_images['alibaba'] = images('alibaba')
@@ -94,3 +115,19 @@ expected_json_servers['microsoft'] = construct_mock_expected_response(mocked_ret
 
 mocked_return_value_servers['oracle'] = servers('oracle')
 expected_json_servers['oracle'] = construct_mock_expected_response(mocked_return_value_servers['oracle'] , 'servers')
+
+#Regions
+mocked_return_value_regions['alibaba'] = [{'name': 'ap-south-1'},{ 'name': 'us-west-1' }]
+expected_json_regions['alibaba'] = {"regions":[{'name': 'ap-south-1'},{ 'name': 'us-west-1' }]}
+
+mocked_return_value_regions['amazon'] = [{'name': 'ap-southeast-2'},{'name': 'ap-south-1'},{'name': 'us-west-1'}]
+expected_json_regions['amazon'] = {"regions":[{'name': 'ap-southeast-2'},{'name': 'ap-south-1'},{'name': 'us-west-1'}]}
+
+mocked_return_value_regions['google'] = [{'name': 'asia-east1'},{'name': 'asia-northeast1'},{'name': 'asia-south1'}, {'name': 'us-west1'}]
+expected_json_regions['google'] = {"regions":[{'name': 'asia-east1'},{'name': 'asia-northeast1'},{'name': 'asia-south1'}, {'name': 'us-west1'}]}
+
+mocked_return_value_regions['microsoft'] = [{'name': 'West US'},{'name': 'Southeast Asia'},{'name': 'southeastasia'}, {'name': 'asiasoutheast'},{'name': 'westus'}, {'name': 'uswest'}]
+expected_json_regions['microsoft'] = {"regions":[{'name': 'West US'},{'name': 'Southeast Asia'},{'name': 'southeastasia'}, {'name': 'asiasoutheast'},{'name': 'westus'}, {'name': 'uswest'}]}
+
+mocked_return_value_regions['oracle'] = []
+expected_json_regions['oracle'] = {"regions":[]}
