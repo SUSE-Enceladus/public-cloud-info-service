3e8268a504cbaba00dd0861194ae41855d1891c6
kberger65 <kberger@suse.com>
2021-04-20 09:48:53 -0400

    backport fixed from fmccarthy

--- next_generation/pint_server/database.py
+++ next_generation/pint_server/database.py
@@ -1,10 +1,26 @@
-# NOTE(gyee): see https://flask.palletsprojects.com/en/1.1.x/patterns/sqlalchemy/
+# NOTE(gyee):
+# see https://flask.palletsprojects.com/en/1.1.x/patterns/sqlalchemy/
 
 import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from pint_server.models import (
+        AlibabaImagesModel,
+        AmazonImagesModel,
+        AmazonServersModel,
+        Base,
+        GoogleImagesModel,
+        GoogleServersModel,
+        ImageState,
+        MicrosoftImagesModel,
+        MicrosoftRegionMapModel,
+        MicrosoftServersModel,
+        OracleImagesModel,
+        ServerType,
+        VersionsModel
+    )
 
 
 def get_environ_or_bust(key_name):
@@ -18,46 +34,46 @@ def create_postgres_url_from_env():
     db_password = get_environ_or_bust('POSTGRES_PASSWORD')
     db_name = get_environ_or_bust('POSTGRES_DB')
     db_host = get_environ_or_bust('POSTGRES_HOST')
+    db_port = os.environ.get('POSTGRES_PORT', 5432)
     db_ssl_mode = os.environ.get('POSTGRES_SSL_MODE', None)
     db_root_cert = os.environ.get('POSTGRES_SSL_ROOT_CERTIFICATE', None)
     ssl_mode = ''
     if db_ssl_mode:
-        # see https://www.postgresql.org/docs/11/libpq-connect.html#LIBPQ-CONNECT-SSLMODE
+        # see
+        # https://www.postgresql.org/docs/11/libpq-connect.html#LIBPQ-CONNECT-SSLMODE
         ssl_mode = '?sslmode=%s' % (db_ssl_mode)
         if db_root_cert:
             ssl_mode += '&sslrootcert=%s' % (db_root_cert)
 
-    return 'postgresql://%(user)s:%(password)s@%(host)s:5432/%(db)s%(ssl)s' % {
-        'user': db_user,
-        'password': db_password,
-        'db': db_name,
-        'host': db_host,
-        'ssl': ssl_mode}
-
-
-if os.environ.get('DATABASE_URI', None):
-    engine = create_engine(os.environ['DATABASE_URI'], convert_unicode=True)
-else:
-    # FIXME(gyee): assume PostgresSQL URI for now. Otherwise, we'll need
-    # to use 'DATABASE_URI' instead.
-    engine = create_engine(
-        create_postgres_url_from_env(), convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-Base.query = db_session.query_property()
+    return 'postgresql://%(user)s:%(password)s@%(host)s:%(port)s/\
+                            %(db)s%(ssl)s' % {
+                                                'user': db_user,
+                                                'password': db_password,
+                                                'db': db_name,
+                                                'host': db_host,
+                                                'port': db_port,
+                                                'ssl': ssl_mode}
 
 
 def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    #from pint_server.models import ImageState, AmazonImagesModel, \
-    #                               OracleImagesModel, \
-    #                               AlibabaImagesModel, MicrosoftImagesModel, \
-    #                               GoogleImagesModel, AmazonServersModel, \
-    #                               MicrosoftServersModel, GoogleServersModel, \
-    #                               ServerType, VersionsModel, \
-    #                               MicrosoftRegionMapModel
+
+    if os.environ.get('DATABASE_URI', None):
+        engine = create_engine(os.environ['DATABASE_URI'],
+                               convert_unicode=True)
+    else:
+        # FIXME(gyee): assume PostgresSQL URI for now. Otherwise, we'll need
+        # to use 'DATABASE_URI' instead.
+        engine = create_engine(
+            create_postgres_url_from_env(), convert_unicode=True)
+
+    db_session = scoped_session(sessionmaker(autocommit=False,
+                                             autoflush=False,
+                                             bind=engine))
+    Base.query = db_session.query_property()
+
     Base.metadata.create_all(bind=engine)
+
+    return db_session
