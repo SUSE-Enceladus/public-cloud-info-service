70e0fd98a6884281e8b18d3c06726b5030f0db64
guangyee <gyee@suse.com>
2021-07-29 03:57:09 +0200

    switch Alembic for schema migration
    This patch also does the following:
    
    1. do not auto create the tables in ORM so we can detect any missing schema
    2. demarcate schema migration/upgrade and data update to satisfy the
       separation of responsibilities security requirement. Schema migration
       will be handled by bin/schema_upgrade.sh while data update will be
       handled by bin/data_update.sh.

---
+++ bin/schema_upgrade.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (c) 2021 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
+
+CurrentDir=$(dirname $(readlink -e $0))
+ProjectRootDir=$(dirname $CurrentDir)
+RepositoryDir=${ProjectRootDir}/pint_server/pint_db_migrate
+
+export PYTHONPATH=${ProjectRootDir}:${PYTHONPATH:+:${PYTHONPATH}}
+
+if [[ "$VIRTUAL_ENV" == "" ]] ; then
+  echo
+  echo "ERROR: python virtualenv not detected. Please run $CurrentDir/create_dev_venv.sh to create and activate the python venv first."
+  echo
+  exit 1
+fi
+
+python3 ${ProjectRootDir}/pint_server/schema_upgrade.py --repository $RepositoryDir $@
+
